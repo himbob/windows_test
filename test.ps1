2a90f007119a1a6b038ad49b17de99b543f43378
Aaron Ruddick <aaron.ruddick@gmail.com>
2020-03-23 15:49:28 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
-Start-Transcript -Path .\log.txt
+Start-Transcript -Path c:\temp\log.txt
 @("1.1.1.1","2.2.2.2") | ForEach {Write-Host $_, "-", ([System.Net.NetworkInformation.Ping]::new().Send($_)).Status}
 Stop-Transcript
